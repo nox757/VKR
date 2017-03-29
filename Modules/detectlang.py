2d1a777cbde2415e320543b641327e133fa21533
nox757 <nox757@gmail.com>
2017-03-30 00:53:27 +0300

    detect lang

--- Modules/detectlang.py
+++ Modules/detectlang.py
@@ -3,25 +3,30 @@ import os, sys
 import string
 from langdetect import detect
 
-path_f = 'k:/Andrew/vkr/example/f1001/'
-path_err = 'k:/Andrew/vkr/example/err2_f1001.txt'
+path_f = 'k:/Andrew/vkr/example/f40k1/'
+path_rem = 'k:/Andrew/vkr/example/remove_f40k1.txt'
 path_out = 'k:/Andrew/vkr/example/f1002/'
 i = 1
-
-
-while i <= 1614:
+list_f = os.listdir(path_f)
+f_rem = open(path_rem, 'w', encoding='utf-8')
+s = ""
+for el in list_f:
    
-        f = open(path_f + str(i) + '.txt', 'r', encoding='utf-8')
-        str00 = f.readline() 
-       
-        str1 = f.readline()
-        str2 = f.read().replace('\n', ' ')
-        str2 = str2.lower()
-        print(i, detect(str(str2)))
-        f.close()
-    
-     
-       
-        i = i + 1
+        f = open(path_f + el,'r', encoding='utf-8')
+        ##readall file
+        fl = 0
+        try:
+                str0 = f.read()
+                if detect(str0) != 'ru':
+                    f.close()
+                    fl = 1
+                    os.remove(path_f+el)
+                    f_rem.write(el + '\n')
+        except:
+                f_rem.write(str(el) +' err\n' )
+                pass
+        if(fl == 0):
+            f.close()
+f_rem.close()
 
 
