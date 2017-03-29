13e5ea7ddee518b3851238048c2b8f804a7ed9d4
nox757 <nox757@gmail.com>
2017-03-30 00:53:51 +0300

    none

---
+++ Modules/try_test.py
@@ -0,0 +1,16 @@
+import sys
+import re
+import os
+import lxml
+from bs4 import BeautifulSoup
+
+i = 0
+path_f = 'k:/Andrew/vkr/example/f40k/'
+
+list_f = os.listdir(path_f)
+
+for el in list_f:
+   print(el)
+
+
+
