ccaf918486363e29342a4e706c8d82ab33d5e4ee
nox757 <nox757@gmail.com>
2017-03-30 16:16:04 +0300

    Create no category doc

--- Modules/try_test.py
+++ Modules/try_test.py
@@ -1,16 +1,54 @@
-import sys
+# import sys
+# import re
+# import os
+# import lxml
+# from bs4 import BeautifulSoup
+#
+# i = 0
+# path_f = 'k:/Andrew/vkr/example/f40k1/'
+#
+# list_f = os.listdir(path_f)
+# el = '1.txt'
+# ##for el in list_f:
+# ##try parse sentences
+# f = open(path_f+el,'r',encoding='utf-8')
+# str_all = f.read()
+# res = re.findall(r"([А-ЯA-Z]((т.п.|т.д.|пр.)|[^?!.\(]|\([^\)]*\))*[.?!])", str_all)
+# print(res)
+# f.close()
+
 import re
-import os
-import lxml
-from bs4 import BeautifulSoup
+import os, sys
+import string
 
-i = 0
-path_f = 'k:/Andrew/vkr/example/f40k/'
 
+path_f = 'k:/Andrew/vkr/example/f40k1/'
+path_err = 'k:/Andrew/vkr/example/err2_f40k2_noC.txt'
+path_out = 'k:/Andrew/vkr/example/f40k2_noC/'
+i = 1
 list_f = os.listdir(path_f)
-
+f_err = open(path_err, 'w', encoding='utf-8')
 for el in list_f:
-   print(el)
+    try:
+        f = open(path_f + el, 'r', encoding='utf-8')
+        str00 = f.readline()
+        str0 = str(re.match(r'\d\d', str00).group  (0))
+        if not os.path.exists(path_out + str0):
+            os.mkdir(path_out + str0)
+        f_out = open(path_out + str0 + '\\' + el, 'w', encoding='utf-8')
+        str1 = f.readline()
+        #f_out.write(str00) # str all name category and code
+        #f_out.write(str1) # str name article
+        str2 = f.read().replace('\n', ' ')
+        str2 = str2.lower()
+        res = re.findall(r"\b([а-яё]+)", str2)
+        str2 = ' '.join(res)
+        f_out.write(str1 + str2 + '\n')
+        f_out.close()
+        f.close()
+    except:
+        f_err.write(str(el) + '\n')
+        pass
+    i = i + 1
 
-
-
+f_err.close()
