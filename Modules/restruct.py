6d889294f54e8921012381031386fae34b490de9
nox757 <nox757@gmail.com>
2017-03-31 01:00:41 +0300

    not Classifier to folder

--- Modules/restruct.py
+++ Modules/restruct.py
@@ -4,7 +4,7 @@ import string
 
 path_f = 'k:/Andrew/vkr/example/f40k1/'
 path_err = 'k:/Andrew/vkr/example/err2_f40k2.txt'
-path_out = 'k:/Andrew/vkr/example/f40k2/'
+path_out = 'k:/Andrew/vkr/example/f40k2_notCl/'
 i = 1
 list_f = os.listdir(path_f)
 f_err = open(path_err, 'w', encoding='utf-8')
@@ -13,12 +13,12 @@ for el in list_f:
         f = open(path_f + el, 'r', encoding='utf-8')
         str00 = f.readline()
         str0 = str(re.match(r'\d\d', str00).group  (0))
-        if not os.path.exists(path_out + str0):
-            os.mkdir(path_out + str0)
-        f_out = open(path_out + str0 + '\\' + el, 'w', encoding='utf-8')
+        if not os.path.exists(path_out):
+            os.mkdir(path_out)
+        f_out = open(path_out  + el, 'w', encoding='utf-8')
         str1 = f.readline()
-        f_out.write(str00) # str all name category and code
-        f_out.write(str1) # str name article
+        f_out.write(str0) # str all name category and code
+        f_out.write(str1.strip()) # str name article
         str2 = f.read().replace('\n', ' ')
         str2 = str2.lower()
         res = re.findall(r"\b([а-яё]+)", str2)
