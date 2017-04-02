fa6f51158e03328d73e25445be32718025e947e7
nox757 <nox757@gmail.com>
2017-04-03 00:13:39 +0300

    Last version read to create new with nouns and adjective

--- Modules/read_csv.py
+++ Modules/read_csv.py
@@ -1,41 +1,46 @@
-import nltk
-from nltk.corpus import stopwords
+
 import pandas as pd
 import csv
 import codecs
 import numpy as np
 import string
 import sys
-import matplotlib.pyplot as plt
-import os
 
+import os
+import pymorphy2 as py
 
 path_f = 'k:/Andrew/vkr/example/f40k_csv_ravn/'
+path_f2 = 'k:/Andrew/vkr/example/f40k_csv_ravn_NA/'
 path_err = 'k:/Andrew/vkr/example/err2_40k2_notCl_pandasToCsv.txt'
 f_err = open(path_err, 'w', encoding='utf-8')
-# mylist = []
-# for chunk in  pd.read_csv('k:/Andrew/vkr/example/40k2_notCl_pandas.csv', encoding='utf-8', chunksize=2000):
-#     mylist.append(chunk)
-# df = pd.concat(mylist, axis= 0)
-# del mylist
-# categ = df['0'].drop_duplicates().values
-# # print(categ)
-# del df[df.columns[0]]
-# print(df.columns[0])
-# df = df.groupby(['0'])
+
 list_f = os.listdir(path_f)
-result = pd.DataFrame()
 
-for el in list_f:###read all data to 'result' 0-GRNTI, 1-Name Article, 2-Text
+
+
+morp = py.MorphAnalyzer()
+for el in list_f[0:5]:###read all data to 'result' 0-GRNTI, 1-Name Article, 2-Text
     try:
+        result = pd.DataFrame()
         df = pd.read_csv(path_f+el, encoding='utf-8',usecols=["0", "1", "2"])
-
         result = result.append(df, ignore_index=True)
-
+        for i in result.index:
+            try:
+                text = result.iat[i, 2].split(' ')
+                mod_text = []
+                for word in text:
+                    morp_list = morp.parse(word)[0].tag
+                    if ('NOUN' in morp_list) or ('ADJF' in morp_list):
+                        mod_text.append(morp.normal_forms(word)[0])
+                result.iat[i, 2] = " ".join(mod_text)
+                print(el +"_"+str(i))
+            except:
+                f_err.write("i"+str(el)+"_"+str(i)+'\n')
+                pass
+        result.to_csv(path_f2 + el, encoding='utf-8')
+        print(el+"_end")
     except:
         f_err.write(str(el)+'\n')
         pass
-#to get only array of values:
-#       result["Name column in qoutes"].values
-print(result[["0","1"]])
 f_err.close()
+
