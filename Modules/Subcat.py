44c6ec8d99205053227ea27b10b6e04352939248
nox757 <nox757@gmail.com>
2017-04-11 00:22:08 +0300

    cat 69 subcat: 09    154 25     63 31     37 51     25 35      4 01      3

--- Modules/Subcat.py
+++ Modules/Subcat.py
@@ -1,7 +1,7 @@
 # import nltk
 # from nltk.corpus import stopwords
 # from sklearn.model_selection import train_test_split
-# import pandas as pd
+import pandas as pd
 # import csv
 # import codecs
 # import numpy as np
@@ -23,7 +23,7 @@ import os
 # import itertools
 import re
 
-path_f = 'k:/Andrew/vkr/example/f40k2/'
+path_f = 'k:/Andrew/vkr/example/f40k2/69/'
 path_err = 'k:/Andrew/vkr/example/err_subcat06.txt'
 path_out = 'k:/Andrew/vkr/example/sub06/'
 
@@ -31,18 +31,23 @@ f_err = open(path_err, 'w', encoding='utf-8')
 list_c = os.listdir(path_f)
 
 i = 0
-for cat in list_c:
-    i = 0
-    list_f = os.listdir(path_f+cat)
-    for el in list_f:
-        try:
-            f = open(path_f +cat+"\\"+ el, 'r', encoding='utf-8')
-            str00 = f.readline()
-            if(re.findall(r'\d\d', str00)[1] != ''):
-                i += 1
-            f.close()
-        except:
-            f_err.write(str(el) + '\n')
-            pass
-    print(i, cat)
+
+list_f = os.listdir(path_f)
+list = []
+for el in list_f:
+    try:
+        f = open(path_f + el, 'r', encoding='utf-8')
+        str00 = f.readline()
+        buff = re.findall(r'\d\d', str00)
+        if(buff[1] != ''):
+            i = i+1
+            list.append([buff[1]])
+        f.close()
+    except:
+        f_err.write(str(el) + '\n')
+        pass
 f_err.close()
+print(i)
+print(list)
+df = pd.DataFrame(data=list, columns=['0'])
+print(df['0'].value_counts())
