96edc683266988975fdfa425708d03a5763ea710
nox757 <nox757@gmail.com>
2017-04-12 01:00:42 +0300

    try parse abstr

--- Modules/try_parse.py
+++ Modules/try_parse.py
@@ -4,21 +4,26 @@ import lxml
 #f = open('k:/Andrew/vkr/example/1.htm', 'rb')
 #lxml_etree_document = html5lib.parse(f, treebuilder="lxml")
 #print (lxml_etree_document)
+import nltk
 from bs4 import BeautifulSoup
-f = open('k:/Andrew/vkr/example/1.htm', 'rb')
+f = open('k:/Andrew/vkr/example/f40k/2.htm', 'rb')
 soup = BeautifulSoup(f, "lxml")
 #print (soup.find('div', 'col-right'))
  
 #print (soup.find('p', itemprop='articleBody'))
 #print (soup.find('ul', 'codes-list'))
+str1 = soup.find('p', itemprop='description').get_text().replace('\n', ' ').strip().lower()
 
-
-for strong_tag in soup.find('ul', 'codes-list').find_all('span'):
-    if strong_tag.text == 'ГРНТИ: ':
-        s = strong_tag.next_sibling.replace(' ','')
-        print(s)
-        ss = re.match(r'\d\d', s)
-        print (ss.group())
-        
-soup1 = soup.find('span', itemprop='headline').get_text().replace('\n', ' ')
-print (soup1)
+print(str1)
+res = re.findall(r"\b([а-яё]+)", str1)
+print(' '.join(res))
+f.close()
+# for strong_tag in soup.find('p', itemprop='description').get_text().replace('\n', ' '):
+#     if strong_tag.text == 'ГРНТИ: ':
+#         s = strong_tag.next_sibling.replace(' ','')
+#         print(s)
+#         ss = re.match(r'\d\d', s)
+#         print (ss.group())
+#
+# soup1 = soup.find('span', itemprop='headline').get_text().replace('\n', ' ')
+# print (soup1)
