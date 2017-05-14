1d1b84adc920b2fd25eba17c95e810f43e4eae25
nox757 <nox757@gmail.com>
2017-05-15 00:26:17 +0300

    full paste add module to read txt, pdf, docx

--- Modules/PreprocessText.py
+++ Modules/PreprocessText.py
@@ -10,6 +10,7 @@ from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from io import StringIO
 
+import docx  ##package pip3 install python-docx
 # input: string with text
 # output: list with token words or None
 def preprocess(text):
@@ -41,7 +42,8 @@ def get_only_NAD(text):
                     newstr.append(morp.normal_forms(word)[0])
         return newstr
 
-#read from pdf file, but problem with some codes differ from 'utf8' ((
+#read from pdf file to string, but problem with some codes differ from 'utf8'
+#                                   and files wtih specific fonts((
 def read_from_pdf(path_f):
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
@@ -55,4 +57,22 @@ def read_from_pdf(path_f):
     retstr.close()
     return str.replace('-\n', '').replace('\n',' ')
 
+def read_from_docx(path_f):
+    doc = docx.Document(path_f)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return '\n'.join(fullText)
 
+
+def read_from_txt(path_f):
+    f = open(path_f, 'r' , errors='ignore')
+    text = f.read()
+    f.close()
+    return  text
+
+pathf = 'k://Andrew//vkr//elib//1.docx'
+
+print(read_from_docx(pathf))
+#enc = chardet.detect(text).get("encoding")
+# print (text)
